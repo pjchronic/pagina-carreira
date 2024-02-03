215023a8510239c83910b40f5f04814f35484840
Paulo Junior <juniorpaulo490@gmail.com>
2024-02-02 22:48:49 -0300

    mensagem de não há vagas com este nome

--- assets/scripts/script_search.js
+++ assets/scripts/script_search.js
@@ -1,5 +1,6 @@
 const search_input = document.getElementById('search');
 const vagas_results = document.getElementById('vagas');
+const noResult = document.getElementById('none');
 
 search_input.addEventListener('keyup', () => {
     let busca = search_input.value.toLowerCase();
@@ -12,9 +13,14 @@ search_input.addEventListener('keyup', () => {
 
             if (textoDoResultado.includes(busca)) {
                 aElement.style.display = 'block'; 
+                noResult.classList.remove('appear');
             } else {
+
                 aElement.style.display = 'none';
+                noResult.classList.add('appear');
+                
             }
         }
     });
+    
 });
